cbdd447999b28efee16d31d52b939065d5154dc0
Urmila More <moreurmila2111@gmail.com>
2023-01-03 12:56:57 +0530

    good
    One day al achieve all things

---
+++ projectgit/src/createrepo/UV.java
@@ -0,0 +1,12 @@
+package createrepo;
+
+public class UV
+{
+     
+	   public static void main(String[] args) 
+	   {
+		
+		   System.out.println(" i have more passions ,i trust on god ");
+		   
+	}
+}
